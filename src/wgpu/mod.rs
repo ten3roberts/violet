96d9a20849206457705ba501173e254230260c6a
Tei Roberts <ten3roberts@gmail.com>
2023-06-17 13:50:08 +0200

    chore: update ci

--- src/wgpu/mod.rs
+++ src/wgpu/mod.rs
@@ -1,9 +1,9 @@
+pub mod components;
 pub mod graphics;
 mod painter;
 mod shape_renderer;
 mod texture;
 pub mod window_renderer;
-pub mod components;
 
 pub use graphics::Gpu;
 pub use shape_renderer::ShapeRenderer;
